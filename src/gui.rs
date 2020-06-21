0a5677286ba033b48680cdc861fd53b41309a74e
Max Whitney <mwhitney@alumni.sfu.ca>
2020-06-21 14:36:17 -0700

    Minor refactoring to GUI code.

--- src/gui.rs
+++ src/gui.rs
@@ -23,10 +23,8 @@ fn draw_stats(ecs: &World, ctx: &mut Rltk) {
 fn draw_gamelog(ecs: &World, ctx: &mut Rltk) {
     let log = ecs.fetch::<GameLog>();
     let mut y = 44;
-    for s in log.entries.iter().rev() {
-        if y < 49 {
-            ctx.print(2, y, s);
-        }
+    for s in log.entries.iter().rev().take(5) {
+        ctx.print(2, y, s);
         y += 1;
     }
 }
@@ -62,32 +60,35 @@ fn draw_tooltips(ecs: &World, ctx: &mut Rltk) {
         }
         width += 3;
 
+        let arrow_pos: Point;
+        let arrow: String;
+        let left_x: i32;
+        let pad_start_x: i32;
+        let pad_delta_x: i32;
         if mouse_pos.0 > 40 {
-            let arrow_pos = Point::new(mouse_pos.0 - 2, mouse_pos.1);
-            let left_x = mouse_pos.0 - width;
-            let mut y = mouse_pos.1;
-            for s in tooltip.iter() {
-                ctx.print_color(left_x, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), s);
-                let padding = (width - s.len() as i32) - 1;
-                for i in 0..padding {
-                    ctx.print_color(arrow_pos.x - i, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &" ".to_string());
-                }
-                y += 1;
-            }
-            ctx.print_color(arrow_pos.x, arrow_pos.y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &"->".to_string());
+            arrow_pos = Point::new(mouse_pos.0 - 2, mouse_pos.1);
+            arrow = "->".to_string();
+            left_x = mouse_pos.0 - width;
+            pad_start_x = arrow_pos.x;
+            pad_delta_x = -1;
         } else {
-            let arrow_pos = Point::new(mouse_pos.0 + 1, mouse_pos.1);
-            let left_x = mouse_pos.0 + 3;
-            let mut y = mouse_pos.1;
-            for s in tooltip.iter() {
-                ctx.print_color(left_x + 1, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), s);
-                let padding = (width - s.len() as i32) - 1;
-                for i in 0..padding {
-                    ctx.print_color(arrow_pos.x + 1 + i, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &" ".to_string());
-                }
-                y += 1;
-            }
-            ctx.print_color(arrow_pos.x, arrow_pos.y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &"<-".to_string());
+            arrow_pos = Point::new(mouse_pos.0 + 1, mouse_pos.1);
+            arrow = "<-".to_string();
+            left_x = mouse_pos.0 + 4;
+            pad_start_x = arrow_pos.x + 1;
+            pad_delta_x = 1;
         }
+        let mut y = mouse_pos.1;
+        for s in tooltip.iter() {
+            ctx.print_color(left_x, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), s);
+            let padding = (width - s.len() as i32) - 1;
+            let mut x = pad_start_x;
+            for _ in 0..padding {
+                ctx.print_color(x, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &" ".to_string());
+                x += pad_delta_x;
+            }
+            y += 1;
+        }
+        ctx.print_color(arrow_pos.x, arrow_pos.y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &arrow);
     }
 }
