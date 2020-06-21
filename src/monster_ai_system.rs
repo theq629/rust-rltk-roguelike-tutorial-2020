4062f911d11f6dbf88fada9c4a92d0c666728c52
Max Whitney <mwhitney@alumni.sfu.ca>
2020-06-20 22:13:33 -0700

    Chapter 7, monsters chase player.

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,22 +1,33 @@
 use specs::prelude::*;
-use super::{Viewshed, Position, Monster, Name};
+use super::{Map, Viewshed, Position, Monster, Name};
 use rltk::{console, Point};
 
 pub struct MonsterAI {}
 
 impl<'a> System<'a> for MonsterAI {
-    type SystemData = (ReadExpect<'a, Point>,
-                       ReadStorage<'a, Viewshed>,
-                       ReadStorage<'a, Position>,
+    type SystemData = (WriteExpect<'a, Map>,
+                       ReadExpect<'a, Point>,
+                       WriteStorage<'a, Viewshed>,
+                       WriteStorage<'a, Position>,
                        ReadStorage<'a, Monster>,
                        ReadStorage<'a, Name>);
 
     fn run(&mut self, data: Self::SystemData) {
-        let (player_pos, viewshed, pos, monster, name) = data;
+        let (mut map, player_pos, mut viewshed, mut pos, monster, name) = data;
 
-        for (viewshed, _pos, _monster, name) in (&viewshed, &pos, &monster, &name).join() {
+        for (mut viewshed, mut pos, _monster, name) in (&mut viewshed, &mut pos, &monster, &name).join() {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(&format!("{} shouts insults", name.name));
+                let path = rltk::a_star_search(
+                    map.xy_idx(pos.x, pos.y) as i32,
+                    map.xy_idx(player_pos.x, player_pos.y) as i32,
+                    &mut *map
+                );
+                if path.success && path.steps.len() > 1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
