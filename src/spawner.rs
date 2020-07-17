3b85cb57875d226522b41357d46371a0c0a34e16
Max Whitney <mwhitney@alumni.sfu.ca>
2020-07-16 17:27:32 -0700

    Decreased monster spawn chance a little.

--- src/spawner.rs
+++ src/spawner.rs
@@ -32,7 +32,7 @@ pub fn spawn_room(ecs: &mut World, room: &Rect) {
 
     {
         let mut rng = ecs.write_resource::<RandomNumberGenerator>();
-        let num_monsters = rng.roll_dice(1, MAX_MONSTERS + 2) - 3;
+        let num_monsters = rng.roll_dice(1, MAX_MONSTERS + 3) - 4;
         let num_items = rng.roll_dice(1, MAX_ITEMS + 2) - 3;
 
         for _ in 0..num_monsters {
