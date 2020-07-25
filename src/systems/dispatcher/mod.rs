c403cad4f1b5629b912cfa173ad06527de927091
Max Whitney <mwhitney@alumni.sfu.ca>
2020-07-25 13:00:35 -0700

    Chapter 73, specs multi-threaded system dispatch.

--- src/systems/dispatcher/mod.rs
+++ src/systems/dispatcher/mod.rs
@@ -1,9 +1,19 @@
 use specs::prelude::World;
 use super::*;
 
+#[cfg(target_arch="wasm32")]
 #[macro_use]
 mod single_thread;
+
+#[cfg(not(target_arch="wasm32"))]
+#[macro_use]
+mod multi_thread;
+
+#[cfg(target_arch="wasm32")]
 pub use single_thread::*;
+
+#[cfg(not(target_arch="wasm32"))]
+pub use multi_thread::*;
 
 pub trait UnifiedDispatcher {
     fn run_now(&mut self, ecs: *mut World);
---
+++ src/systems/dispatcher/multi_thread.rs
@@ -0,0 +1,42 @@
+use specs::prelude::*;
+use super::UnifiedDispatcher;
+
+pub struct MultiThreadedDispatcher {
+    pub dispatcher: specs::Dispatcher<'static, 'static>
+}
+
+impl<'a> UnifiedDispatcher for MultiThreadedDispatcher {
+    fn run_now(&mut self, ecs: *mut World) {
+        unsafe {
+            self.dispatcher.dispatch(&mut *ecs);
+        }
+    }
+}
+
+macro_rules! construct_dispatcher {
+    (
+        $(
+            (
+                $type:ident,
+                $name:expr,
+                $deps:expr
+            )
+        ),*
+    ) => {
+        fn new_dispatch() -> Box<dyn UnifiedDispatcher + 'static> {
+            use specs::DispatcherBuilder;
+
+            let dispatcher = DispatcherBuilder::new()
+                $(
+                    .with($type{}, $name, $deps)
+                )*
+                .build();
+
+            let dispatch = MultiThreadedDispatcher{
+                dispatcher: dispatcher
+            };
+
+            return Box::new(dispatch);
+        }
+    };
+}
