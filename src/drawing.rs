61a87127653dd5782329ff8264dd747cb0b5fe54
Max Whitney <mwhitney@alumni.sfu.ca>
2020-07-25 14:56:10 -0700

    Move drawing code to its own file.

---
+++ src/drawing.rs
@@ -0,0 +1,61 @@
+use specs::prelude::*;
+use rltk::prelude::*;
+use super::{Position, Renderable};
+use super::map::{Map, TileType};
+
+pub fn draw_world(ecs: &World, ctx: &mut Rltk) {
+    draw_map(ecs, ctx);
+    draw_entities(ecs, ctx);
+}
+
+fn draw_map(ecs: &World, ctx: &mut Rltk) {
+    let map = ecs.fetch::<Map>();
+
+    let mut y = 0;
+    let mut x = 0;
+    for (idx, tile) in map.tiles.iter().enumerate() {
+        let glyph;
+        let mut fg;
+        if map.revealed_tiles[idx] {
+            match tile {
+                TileType::Floor => {
+                    glyph = rltk::to_cp437('.');
+                    fg = RGB::from_f32(0.0, 0.5, 0.5);
+                },
+                TileType::Wall => {
+                    glyph = rltk::to_cp437('#');
+                    fg = RGB::from_f32(0., 1.0, 0.);
+                },
+                TileType::DownStairs => {
+                    glyph = rltk::to_cp437('>');
+                    fg = RGB::from_f32(0., 1.0, 1.0);
+                }
+            }
+            if !map.visible_tiles[idx] {
+                fg = fg.to_greyscale()
+            }
+            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
+        }
+        x += 1;
+        if x >= map.width {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+fn draw_entities(ecs: &World, ctx: &mut Rltk) {
+    let map = ecs.fetch::<Map>();
+    let positions = ecs.read_storage::<Position>();
+    let renderables = ecs.read_storage::<Renderable>();
+
+    let mut data = (&positions, &renderables).join().collect::<Vec<_>>();
+    data.sort_by(|&a, &b| b.1.render_order.cmp(&a.1.render_order));
+
+    for (pos, render) in data.iter() {
+        let idx = map.xy_idx(pos.x, pos.y);
+        if map.visible_tiles[idx] {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate serde;
-use rltk::{Rltk, GameState, RGB, Point};
+use rltk::{Rltk, GameState, Point};
 use specs::prelude::*;
 use specs::saveload::{SimpleMarker, SimpleMarkerAllocator};
 
@@ -11,6 +11,7 @@ mod player;
 pub use player::*;
 mod rect;
 pub use rect::*;
+mod drawing;
 mod gui;
 mod gamelog;
 mod spawner;
@@ -178,8 +179,7 @@ impl GameState for State {
         match newrunstate {
             RunState::MainMenu{..} => {}
             _ => {
-                draw_map(&self.ecs, ctx);
-                draw_entities(&self.ecs, ctx);
+                drawing::draw_world(&self.ecs, ctx);
                 gui::draw_ui(&self.ecs, ctx);
             }
         }
@@ -300,58 +300,6 @@ impl GameState for State {
         }
 
         delete_the_dead(&mut self.ecs);
-    }
-}
-
-fn draw_map(ecs: &World, ctx: &mut Rltk) {
-    let map = ecs.fetch::<Map>();
-
-    let mut y = 0;
-    let mut x = 0;
-    for (idx, tile) in map.tiles.iter().enumerate() {
-        let glyph;
-        let mut fg;
-        if map.revealed_tiles[idx] {
-            match tile {
-                TileType::Floor => {
-                    glyph = rltk::to_cp437('.');
-                    fg = RGB::from_f32(0.0, 0.5, 0.5);
-                },
-                TileType::Wall => {
-                    glyph = rltk::to_cp437('#');
-                    fg = RGB::from_f32(0., 1.0, 0.);
-                },
-                TileType::DownStairs => {
-                    glyph = rltk::to_cp437('>');
-                    fg = RGB::from_f32(0., 1.0, 1.0);
-                }
-            }
-            if !map.visible_tiles[idx] {
-                fg = fg.to_greyscale()
-            }
-            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
-        }
-        x += 1;
-        if x >= map.width {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
-fn draw_entities(ecs: &World, ctx: &mut Rltk) {
-    let map = ecs.fetch::<Map>();
-    let positions = ecs.read_storage::<Position>();
-    let renderables = ecs.read_storage::<Renderable>();
-
-    let mut data = (&positions, &renderables).join().collect::<Vec<_>>();
-    data.sort_by(|&a, &b| b.1.render_order.cmp(&a.1.render_order));
-
-    for (pos, render) in data.iter() {
-        let idx = map.xy_idx(pos.x, pos.y);
-        if map.visible_tiles[idx] {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
     }
 }
 
